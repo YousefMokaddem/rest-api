8625c41224c75f29ae8419583e2cd736ba55a726
Yousef Mokaddem <yousefmokaddem96@gmail.com>
2019-02-21 02:41:36 -0500

    Create route.js with authentication and users route

---
+++ routes.js
@@ -0,0 +1,63 @@
+'use strict'
+
+const express = require('express');
+const router = express.Router();
+const User = require('./models').User;
+const Course = require('./models').Course;
+const bcryptjs = require('bcryptjs');
+const auth = require('basic-auth');
+
+const authenticateUser = async (req,res,next) => {
+
+    let message = null;
+
+    //parse user credentials from auth header
+    const credentials = auth(req);
+
+    //if the credentials are available
+    if(credentials){
+        //find the user by their email address (the 'key' from the auth header)
+        
+        let user = (await User.find({emailAddress: credentials.name}))[0];
+        //if the user was retrieved...
+        if(user){
+            console.log(user);
+            console.log(credentials);
+            const authenticated = bcryptjs
+                .compareSync(credentials.pass, user.password);
+            //if the passwords match...
+            if(authenticated){
+                req.currentUser = user;
+            }else{
+                message = `Authentication failure for username: ${user.emailAddress}`;
+            }
+        }else{
+            message = `User not found: ${credentials.name}`;
+        }
+    }else{
+        message = `Authentication header not found`
+    }
+
+    //if authentication failed
+    if(message){
+        console.warn(message);
+
+        //return 401 unauthorized
+        res.status(401).json({message: 'Access Denied'});
+    }else{
+        //successful authentication passes control to the route handler
+        next();
+    }
+}
+
+// route to return currently authenticated user
+router.get('/users', authenticateUser, (req,res) => {
+    let user = req.currentUser;
+    res.json({
+        firstName: user.firstName,
+        lastName: user.lastName,
+        emailAddress: user.emailAddress
+    });
+});
+
+module.exports = router;
