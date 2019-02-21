b1ebeddd4fc8e13715b798dda5e3fd74089848e9
Yousef Mokaddem <yousefmokaddem96@gmail.com>
2019-02-21 01:24:29 -0500

    Create mongoose schema and models

--- app.js
+++ app.js
@@ -3,6 +3,23 @@
 // load modules
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+
+// set up mongoose connection
+mongoose.connect('mongodb://localhost:27017/fsjstd-restapi',{useNewUrlParser: true});
+
+var db = mongoose.connection;
+
+db.on("error", function(err){
+	console.error("connection error:", err);
+});
+
+db.once("open", function(){
+	console.log("db connection successful");
+});
+
 
 // variable to enable global error logging
 const enableGlobalErrorLogging = process.env.ENABLE_GLOBAL_ERROR_LOGGING === 'true';
@@ -14,6 +31,7 @@ const app = express();
 app.use(morgan('dev'));
 
 // TODO setup your api routes here
+app.use("/api", routes);
 
 // setup a friendly greeting for the root route
 app.get('/', (req, res) => {
